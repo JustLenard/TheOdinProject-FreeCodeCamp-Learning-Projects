d2959fd82e09c892fefcf83051a9f82ebfa59d0d
Len <justlenard.justme@gmail.com>
2021-10-20 16:30:30 +0300

    Finished the console version or Rock-Paper-Scissors in JS

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,21 +1,38 @@
 choices = ['Rock', 'Paper', 'Scissors']
-win = [['Rock', 'Scissors'], ['Paper', 'Rock'], ['Scissors', 'Paper']]
-lose = [['Scissors', 'Rock'], ['Rock', 'Paper'], ['Paper', 'Scissors']]
+win = ['RockScissors', 'PaperRock', 'ScissorsPaper']
+lose = ['ScissorsRock', 'RockPaper', 'PaperRock']
 
-let computerChoice = choices[Math.floor(Math.random() * choices.length)]
-//let humanChoice = prompt()
 
-console.log(computerChoice)
-console.log('Herro')
-//console.log(humanChoice)
-
-for (let i = 0; i < 5; i++){
-    console.log('The for loop is working')
+let computerPoints = humanPoints = 0
+for (i = 1; i < 6; i++){
+    console.log(`Round: ${i}`)
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    let humanChoice = prompt()
+    console.log(`Computer choice is: ${computerChoice}`)
+    if (humanChoice === computerChoice){
+        console.log(`Tie round. The score is ${humanPoints}:${computerPoints}`)
+    }
+    else if (win.includes(humanChoice + computerChoice)) {
+        humanPoints++;
+        console.log(`Human wins round. The score is ${humanPoints}:${computerPoints}`);
+    }
+    else if (lose.includes(humanChoice + computerChoice)){
+        computerPoints++;
+        console.log(`Computer wins round. The score is ${humanPoints}:${computerPoints}`);
+    }
+    else {
+        console.log("Please type 'Rock', 'Paper', or 'Scissors'.");
+        i--;
+    }
 }
 
-console.log(lose)
-
-//console.lost(['Rock', 'Scissors'] === win[0])
-console.log(win[0])
-
-console.log([1, 2] === [1, 2])
+console.log(`Final Result: ${humanPoints}:${computerPoints}`)
+if (humanPoints === computerPoints){
+    console.log('The game is a tie.')
+}
+else if (humanPoints > computerPoints) {
+    console.log('Human wins the Game')
+}
+else {
+    console.log('Computer wins the Game')
+}
