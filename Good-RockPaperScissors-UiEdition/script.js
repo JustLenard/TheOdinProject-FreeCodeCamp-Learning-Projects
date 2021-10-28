7b79e388092e20adc86fee4e92e0db3e0e52f9af
Len <justlenard.justme@gmail.com>
2021-10-28 16:40:57 +0300

    some JS

--- Good-RockPaperScissors-UiEdition/script.js
+++ Good-RockPaperScissors-UiEdition/script.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+/*const paper = document.querySelector('#paper')
+
+paper.addEventListener('click', function (e) {
+    console.log(e);
+    paper.classList.add('click');
+});*/
+
+const hands = document.querySelectorAll('.hand-box')
+const humanChoice = document.querySelector('.human-choice')
+
+hands.forEach(hand => hand.addEventListener('click', function (e) {
+    hands.forEach(nothand => nothand.classList.remove('click'));
+    console.log(e);
+    humanChoice.setAttribute('id', 'human-choice')
+    hand.classList.add('click');
+}));
