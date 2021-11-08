3e5890f2bdc43caad535538e52bed6a1cf59d70f
Len <justlenard.justme@gmail.com>
2021-11-08 16:21:58 +0200

    Holy fuck man. The calculator is WAY more fucking convoluted to do than I thought. The fact that I am doing it with JS doesn't help what so ever. Freaking 'null' and '' values. Give me my Python back

--- ★Good-Calculator/script.js
+++ ★Good-Calculator/script.js
@@ -1,23 +1,93 @@
+const numbers = '1234567890.';
+const operations = '+-*/^';
+
 let passive = document.querySelectorAll('.passive');
 let active = document.querySelectorAll('.active');
 
-let result = '';
+const deleteBtn = document.querySelector('.delete');
+const clearBtn = document.querySelector('.clear');
+
 let toCalculate = 0;
+let expression = '';
+var result;
+
+clearBtn.addEventListener('click', () => {
+	expression = '';
+});
 
 passive.forEach(button => {
 	button.addEventListener('click', () => {
-		toCalculate += button.textContent;
-		console.log(toCalculate);
-		console.log('f');
-		calculateThis();
-		result = calculateThis();
-		console.log(four);
+		if (numbers.includes(button.textContent)) {
+			expression += button.textContent;
+		} else if (operations.includes(button.textContent)) {
+			expression += ` ${button.textContent} `;
+		}
+		expression = expression.replace('*  *', '^');
+		console.log(`Expression: ${expression}`);
+		// console.log(expression.split(' '));
+		// console.log(expression.split(' ').length);
+		calculateThis(expression);
+		// result = calculateThis();
 	});
 });
 
-console.log(2 + (2 * 3) / 4);
+function calculateThis(expression) {
+	result = expression.split(' ');
+	console.log(`This is before power: ${result}`);
+	power(result);
+	console.log(`This is after power: ${result}`);
+	result = result.filter(el => el !== null && el !== '');
+	// var f = result.filter(el => el);
+	console.log(result);
+	// console.log(typeof result);
+	// console.log(`This is before multiply: ${result}`);
+	// multiply(result);
+	// console.log(`This is after multiply: ${result}`);
 
-function calculateThis(toCalculate, x) {
-	console.log('result');
-	return 4;
+	// while (
+	// 	expression.split(' ').length !== 1 &&
+	// 	expression.split(' ').length !== 0
+	// ) {
+	// }
+
+	// for (i = 0; i < result.length; i++) {
+	// 	if (result[i] === '^') {
+	// 		// console.log(expression.indexOf('^'));
+	// 		i = result.indexOf('^');
+	// 		let toReplaceWith = result[i - 1] ** result[i + 1];
+	// 		result[i + 1] = toReplaceWith;
+	// 		result.splice(i - 1, i + 1);
+	// 		console.log(`result in for loop: ${result}`);
+	// 		console.log(toReplaceWith);
+	// 	}
+	// 	// console.log(`result after if: ${result}`);
+	// }
 }
+
+function power(result) {
+	if (result.indexOf('^') !== -1 && result.length > result.indexOf('^')) {
+		i = result.indexOf('^');
+		let toReplaceWith = result[i - 1] ** result[i + 1];
+		result[i] = toReplaceWith.toString();
+		// result.splice(i - 1, i + 1);
+		result[i + 1] = '';
+		result[i - 1] = '';
+		if (result.indexOf('^') !== -1) {
+			power(result);
+		}
+	}
+}
+
+function multiply(result) {
+	if (result.indexOf('*') !== -1) {
+		i = result.indexOf('^');
+		let toReplaceWith = result[i - 1] * result[i + 1];
+		result[i] = toReplaceWith;
+		result.splice(i - 1);
+		if (result.indexOf('*') !== -1) {
+			multiply(result);
+		}
+	}
+}
+
+// function remove
