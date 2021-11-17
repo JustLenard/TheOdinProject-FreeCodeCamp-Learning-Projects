28e5d7e833a386597326dc003362f2dffd2e6f66
Len <justlenard.justme@gmail.com>
2021-11-17 16:30:57 +0200

    Wrote a bit of everything. I think I will write the game logic first is some dirty JS and then will rewrite the entire thing the OOP way. It's kind of too hard to do it good the first time

--- ★Good-Tic-Tac-Toe/script.js
+++ ★Good-Tic-Tac-Toe/script.js
@@ -1,1 +1,54 @@
-console.log('Herro');
+const dificultyChoices = document.querySelectorAll('[data-dificutly]');
+const markerChoices = document.querySelectorAll('[data-marker]');
+const playingField = document.querySelectorAll('[data-position]');
+const restart = document.querySelector('.restart');
+
+dificultyChoices.forEach(choice => {
+	choice.addEventListener('click', () => {
+		dificultyChoices.forEach(choice => {
+			choice.classList.remove('active');
+		});
+		choice.classList.add('active');
+	});
+});
+
+markerChoices.forEach(choice => {
+	choice.addEventListener('click', () => {
+		markerChoices.forEach(choice => {
+			choice.classList.remove('active');
+		});
+		choice.classList.add('active');
+	});
+});
+
+playingField.forEach(choice => {
+	choice.addEventListener('click', () => {
+		playingField.forEach(choice => {
+			choice.classList.remove('active');
+		});
+		choice.classList.add('active');
+		choice.textContent = 'x';
+		aiMove();
+	});
+});
+
+restart.addEventListener('click', () => {
+	playingField.forEach(choice => {
+		choice.classList.remove('active');
+		choice.textContent = '';
+	});
+});
+
+function aiMove() {
+	randNum = randomNumber();
+	console.log(playingField[randNum].textContent);
+	if (playingField[randNum].textContent === '') {
+		playingField[randNum].textContent = 'o';
+	} else if (playingField[randNum].textContent !== '') {
+		aiMove();
+	}
+}
+
+function randomNumber() {
+	return Math.floor(Math.random() * 10);
+}
