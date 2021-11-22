70ae5247d47ce53c7d4fe51b2b17aeafaaa12ad8
Len <justlenard.justme@gmail.com>
2021-11-22 15:37:18 +0200

    why in the heck OOP is so damn hard

--- ★Good-Tic-Tac-Toe/minimax.py
+++ ★Good-Tic-Tac-Toe/minimax.py
@@ -1,1 +1,19 @@
-print('hello')
+class Tic_Tac_Toe:
+    x = 2
+    
+    
+    def __init__(self):
+        self.board = self.make_board()
+        self.placeholder = self.show_position(self.board)
+        
+    def make_board(self):
+        return [' ' for _ in range(9)]
+        
+    def show_position(self, f):
+        return [x for x in f]
+
+f = Tic_Tac_Toe()
+
+
+print(f.show_position)
+print(f.board)
