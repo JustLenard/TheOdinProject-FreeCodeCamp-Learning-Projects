8d9df2684eb0fc719c02cd863d4495f5bcafb356
Len <justlenard.justme@gmail.com>
2021-11-24 21:42:40 +0200

    added win condition

--- ★Good-Tic-Tac-Toe/minimax.py
+++ ★Good-Tic-Tac-Toe/minimax.py
@@ -1,5 +1,5 @@
 board = ['_' for x in range(9)]
-wind_conditions = []
+win_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 player = 'x'
 ai = 'o'
 
@@ -7,7 +7,6 @@ def print_board(board):
     print(f'{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}\n')
     print(f'{board[0]} {board[1]} {board[2]}\n{board[3]} {board[4]} {board[5]}\n{board[6]} {board[7]} {board[8]}\n')
 
-print_board(board)
 
 def check_empty(board):
     for postion in board:
@@ -16,7 +15,10 @@ def check_empty(board):
     return False
 
 def check_win(board):
-    
+    for condition in win_conditions:
+        if board[condition[0]] + board[condition[1]] + board[condition[2]] in ['xxx','ooo']:
+            return board[condition[0]]
+    return False
 
 def player_input(board):
     player_move = input('Type the positin for "o" or 9 to exit: ')
@@ -28,8 +30,11 @@ def player_input(board):
         board = ['_' for x in range(9)]
     else:
         print('Please give a valid input.')
-        print_board(board)
     print_board(board)
+    winner = check_win(board)
+    if winner != False:
+        print(f'The winner is {winner}')
+        return
     if check_empty(board) == True:
         player_input(board)
 
