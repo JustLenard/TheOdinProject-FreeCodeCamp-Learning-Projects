56288a2ffee742ffdc0e1048d3534a8b1e8f6c8b
Len <justlenard.justme@gmail.com>
2021-11-24 21:02:00 +0200

    started making the console tic tac toe with python and without OOP. One step at a time. I am making progress again. Feels good.

--- ★Good-Tic-Tac-Toe/minimax.py
+++ ★Good-Tic-Tac-Toe/minimax.py
@@ -1,22 +1,36 @@
-class Tic_Tac_Toe:
-    x = 2
-    board = [' ' for _ in range(9)]
+board = ['_' for x in range(9)]
+wind_conditions = []
+player = 'x'
+ai = 'o'
+
+def print_board(board):
+    print(f'{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}\n')
+    print(f'{board[0]} {board[1]} {board[2]}\n{board[3]} {board[4]} {board[5]}\n{board[6]} {board[7]} {board[8]}\n')
+
+print_board(board)
+
+def check_empty(board):
+    for postion in board:
+        if postion == '_':
+            return True
+    return False
+
+def check_win(board):
     
-    
-    def __init__(self):
-        # self.board = self.make_board()
-        # self.updade_board = self.print_board(self.board)
-        pass
-        
-    def print_board(self, board):
-        print(f'{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}\n')
-        print(f'{board[0]} {board[1]} {board[2]}\n{board[3]} {board[4]} {board[5]}\n{board[6]} {board[7]} {board[8]}\n')
 
-    print_boar = print_board(board)
-    # f'{} {} {}\n{} {} {}\n{} {} {}\n'
+def player_input(board):
+    player_move = input('Type the positin for "o" or 9 to exit: ')
+    if player_move in [str(x) for x in range(9)]:
+        board[int(player_move)] = player
+    elif player_move == '9':
+        return
+    elif player_move == 'restart':
+        board = ['_' for x in range(9)]
+    else:
+        print('Please give a valid input.')
+        print_board(board)
+    print_board(board)
+    if check_empty(board) == True:
+        player_input(board)
 
-game = Tic_Tac_Toe()
-
-game.board[3] = 3
-print(game.board)
-print(game.print_board())
+player_input(board)
