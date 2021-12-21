d737475d49091cd412ded69f6f23726fd1370901
Len <justlenard.justme@gmail.com>
2021-12-21 16:29:57 +0200

    Some more stuff

--- Playing with react/react-task-tracker/src/App.js
+++ Playing with react/react-task-tracker/src/App.js
@@ -1,9 +1,25 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Doctor appointment',
+			day: 'Feb 5th 2:30pm',
+			reminder: true,
+		},
+	]);
+
+	//Delete task
+	const deleteTask = id => {
+		console.log('deleteTask', id);
+	};
 	return (
 		<div className="container">
 			<Header />
+			<Tasks tasks={tasks} onDelete={deleteTask} />
 		</div>
 	);
 }
