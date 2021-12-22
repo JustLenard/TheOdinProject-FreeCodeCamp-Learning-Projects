931f3870068d592a5d68a41cbb4cdda92a152c39
Len <justlenard.justme@gmail.com>
2021-12-22 15:38:51 +0200

    I already lost track of what and how I am doing. I will return later to this project

--- Playing with react/react-task-tracker/src/App.js
+++ Playing with react/react-task-tracker/src/App.js
@@ -14,12 +14,22 @@ function App() {
 
 	//Delete task
 	const deleteTask = id => {
-		console.log('deleteTask', id);
+		setTasks(tasks.filter(task => task.id !== id));
 	};
+
+	// Toggle reminder
+	const toggleReminder = id => {
+		console.log(id);
+	};
+
 	return (
 		<div className="container">
 			<Header />
-			<Tasks tasks={tasks} onDelete={deleteTask} />
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+			) : (
+				'No tasks to show'
+			)}
 		</div>
 	);
 }
