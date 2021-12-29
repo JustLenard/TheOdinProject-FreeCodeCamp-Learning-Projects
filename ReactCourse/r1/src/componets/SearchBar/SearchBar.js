df2834e0191bab0247f527d07a303a93a990b6a2
Len <justlenard.justme@gmail.com>
2021-12-29 16:14:21 +0200

    I am sick.

--- ReactCourse/r1/src/componets/SearchBar/SearchBar.js
+++ ReactCourse/r1/src/componets/SearchBar/SearchBar.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 
+const numbers = ['1', '2', '3', '4'];
+
 const SearchBar = () => {
-	const [searchValue, setSearchValue] = useState('what do you want to search?');
+	const [searchValue, setSearchValue] = useState('');
 	const handleInputChange = event => {
 		setSearchValue(event.target.value);
 	};
 
+	const handleClearClick = () => {
+		setSearchValue('');
+	};
+
+	const shouldDisplayButton = searchValue.length > 0;
+	const filteredProducts = numbers.filter(product => {
+		return product.includes(searchValue);
+	});
+
 	return (
 		<div>
 			<input type="text" value={searchValue} onChange={handleInputChange} />
+			{shouldDisplayButton && <button onClick={handleClearClick}>clear</button>}
+			<ul>
+				{filteredProducts.map(number => {
+					return <li key={number}>{number}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
